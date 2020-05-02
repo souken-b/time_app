bfaf67830f48c408c14859ed6c752dc0ecd64e74
souken-b <yuto430scuderia@gmail.com>
2020-05-02 19:36:20 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Timer></Timer>
   </div>
 </template>
 
+<script>
+import Timer from './components/Timer.vue'
+
+export default {
+  components: {
+    Timer
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    margin-top: 100px;
+  }
 </style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="timer">
+    <div class="timer">
+      <div class="time">
+        {{ formatTime }}
+      </div>
+      <button @click="start" v-if="!timerOn">Start</button>
+      <button @click="stop" v-if="timerOn">Stop</button>
+      <button @click="reset">Reset</button>
+
+      <div v-show="alert">
+        <button>open slack</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'timer',
+  data () {
+    return {
+      min: 0,
+      sec: 10,
+      timerOn: false,
+      timerObj: null,
+      alert: false
+    }
+  },
+  methods: {
+    count: function () {
+      if (this.sec <= 0 && this.min >= 1) {
+        this.min--
+        this.sec = 59
+      } else if (this.sec <= 0 && this.min <= 0) {
+        this.complete()
+      } else {
+        this.sec--
+      }
+    },
+    start: function () {
+      const self = this
+      this.timerObj = setInterval(function () { self.count() }, 1000)
+      this.timerOn = true
+      this.alert = false
+    },
+    stop: function () {
+      clearInterval(this.timerObj)
+      this.timerOn = false
+      this.alert = false
+    },
+    complete: function () {
+      clearInterval(this.timerObj)
+      this.alert = true
+    },
+    reset: function () {
+      this.min = 0
+      this.sec = 10
+      this.alert = false
+    }
+  },
+  computed: {
+    // eslint-disable-next-line vue/return-in-computed-property
+    formatTime: function () {
+      const timeStrings = [
+        this.min.toString(),
+        this.sec.toString()
+      ].map(function (str) {
+        if (str.length < 2) {
+          return '0' + str
+        } else {
+          return str
+        }
+      })
+      return timeStrings[0] + ':' + timeStrings[1]
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+#timer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: aqua;
+}
+.time {
+  font-size: 100px;
+}
+</style>
